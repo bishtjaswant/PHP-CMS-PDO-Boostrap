584d4adc587fda9b8a9ee968328f6eb08962eb61
bishtjaswant <jaswantbishtuk1995@gmail.com>
2019-05-01 20:26:11 +0530

    php cms project

--- admin/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php 
-/* pdo connection */
-try {
-	$conn = new PDO("mysql:host=localhost;dbname=newsportal;", "root","") ;
-	$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-	 
-} catch (PDOException $e) {
-	die('error'.$e->getMessage());
-}
--- pass.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-echo password_hash("jaswant", PASSWORD_BCRYPT);
-
-
- ?>
